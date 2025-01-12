98b16a2df37ef88020dc7a7db435b673373c16c5
peafield <peterjohncoles@gmail.com>
2025-01-12 22:01:20 +0000

    Add BlueSky icon component and integrate react-icons; update Navbar with social links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,19 @@
-import Link from "next/link";
-import DarkModeToggle from "./buttons/DarkModeToggle";
+"use client";
 
+import { motion } from "framer-motion";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import DarkModeToggle from "./buttons/DarkModeToggle";
+import { RiBlueskyLine, RiGithubFill } from "react-icons/ri";
 const Navbar = () => {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  const logoText = "Peafield.dev".split("");
+
   const navLinks = [
     { id: 1, text: "Blog", path: "/blog" },
     { id: 2, text: "Portfolio", path: "/portfolio" },
@@ -12,21 +24,39 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center p-4">
         {/* Home */}
         <div>
-          <Link
-            href="/"
-            className="font-openSans font-bold text-black dark:text-white"
-          >
-            Peafield.dev
+          <Link href="/">
+            <motion.span className="font-openSans font-medium text-black dark:text-white">
+              {logoText.map((letter, index) => (
+                <motion.span
+                  key={index}
+                  initial={{ opacity: 1, y: -20 }}
+                  animate={{
+                    opacity: [1, 0, 1],
+                    y: [-20, 0, -20],
+                  }}
+                  transition={{
+                    delay: index * 0.1,
+                    duration: 0.5,
+                    repeat: Infinity,
+                    repeatType: "loop",
+                    repeatDelay: 8,
+                  }}
+                >
+                  {letter}
+                </motion.span>
+              ))}
+            </motion.span>
           </Link>
         </div>
+
         {/* Nav links */}
         <div>
-          <ul className="flex space-x-8">
+          <ul className="flex gap-x-8">
             {navLinks.map((link) => (
               <li key={link.id}>
                 <Link
                   href={link.path}
-                  className="font-openSans font-medium text-black dark:text-white"
+                  className="font-openSans font-medium text-black dark:text-white hover:text-gray-700 hover:dark:text-gray-300"
                 >
                   {link.text}
                 </Link>
@@ -35,7 +65,13 @@ const Navbar = () => {
           </ul>
         </div>
         {/* Socials and utils */}
-        <div>
+        <div className="flex gap-x-4 items-center justify-center">
+          <Link href="https://bsky.app/profile/peafield.dev">
+            <RiBlueskyLine className="size-6 hover:text-gray-700 hover:dark:text-gray-300" />
+          </Link>
+          <Link href="https://github.com/Peafield">
+            <RiGithubFill className="size-6 hover:text-gray-700 hover:dark:text-gray-300" />
+          </Link>
           <DarkModeToggle />
         </div>
       </div>
