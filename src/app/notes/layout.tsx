ef6b47beee347f86c69913f5cd68887c0aa0ba0e
peafield <peterjohncoles@gmail.com>
2025-06-02 12:05:30 +0100

    feat: implement MDX components and create notes layout with sample content

---
+++ src/app/notes/layout.tsx
@@ -0,0 +1,6 @@
+import Container from "@/components/Container";
+import React from "react";
+
+export default function NotesLayout({children}: {children: React.ReactNode}) {
+    return <Container>{children}</Container>
+}
--- src/app/notes/page.tsx
+++
@@ -1,7 +0,0 @@
-import TempPage from "@/components/TempPage";
-
-const NotesPage = () => {
-  return <TempPage />;
-};
-
-export default NotesPage;
